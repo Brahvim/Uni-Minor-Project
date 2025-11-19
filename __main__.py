5956099ce8d1a55c429848fa8299b0c85cf605e3
Brahvim <69293652+Brahvim@users.noreply.github.com.>
2025-11-20 02:01:35 +0530

    Yoooo! It... detects! ...Now we Qwen-OCR!

--- __main__.py
+++ __main__.py
@@ -1,152 +1,107 @@
-# from ultralytics import YOLO
+from ultralytics import YOLO
 from queue import Queue
-import netifaces as ni
 import numpy as np
-import websockets
-import subprocess
-import ipaddress
 import threading
+import websocket
 import requests
-import asyncio
-import json
-import sys
 import cv2
 
 # region Vars.
-s_secrets = {}
-
-with open("./secrets.json") as f:
-    s_secrets.update(json.load(f))
-
-s_pathYolos = "./yolov11-license-plate-detection"
-s_pathJpegs = "./photos"
-
 s_queueEspPayloads = Queue()
 s_queuePlates = Queue()
 s_queueJpegs = Queue()
+s_queueFiles = Queue()
 
-s_espIp = ""
-s_espMac = s_secrets["mac"]
-# s_yolo = YOLO(f"{s_pathYolos}/license-plate-finetune-v1n.pt")
-# endregion
-
-# region Utils.
-
-
-def utilEspIpPingNets(p_iface: str = "wlo1"):
-    """
-    [UNUSED!] Force ping everyone on yo' nets!
-    """
-    for ip in ipaddress.ip_network(ni.ifaddresses(p_iface)[ni.AF_INET][0]["gateway"], strict=False).hosts():
-        ipstr = str(ip)
-        # print(f"Pinging `{ipstr}`...")
-        subprocess.Popen(["ping", "-c", "1", "-W", "1", ipstr])
-
-
-def utilEspIpNeigh(p_mac: str = s_espMac):
-    def doit():
-        arp = subprocess.check_output(["ip", "neighbour"], text=True)
-        for l in arp.splitlines():
-            if p_mac in l.lower():
-                return l
-
-    # if not doit():
-        # print("ESP32-CAM not in `ip neighbour` logs! Pinging ALL subnets...")
-        # utilEspIpPingNets()
-    return doit()
-
-
-def utilEspIpScan(p_mac: str = s_espMac, p_iface: str = "wlo1"):
-    p_mac = p_mac.lower()
-    request = ARP(pdst="192.168.1.0/24")
-    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = broadcast / request
-
-    ans = srp(packet, iface=p_iface, timeout=2, verbose=0)[0]
-
-    for _, r in ans:
-        if r.hwsrc.lower() == p_mac:
-            s_espIp = r.psrc
-            return r.psrc
-
-    return None
-
-
-def utilEspIpFind(p_mac: str = s_espMac):
-    p_mac = p_mac.lower()
-
-    with open("/proc/net/arp") as f:
-        for line in f.readlines()[1:]:
-            cols = line.split()
-            ip = cols[0]
-            mac = cols[3].lower()
-            if mac == p_mac:
-                return ip
-
-    return None
-
-
+s_espIpStr = ""
+s_pathJpegs = "./photos"
+s_pathYolos = "./yolov11-license-plate-detection"
+s_yolo = YOLO(f"{s_pathYolos}/license-plate-finetune-v1n.pt")
 # endregion
 
 # region Workers.
 
-def workerThreadDecode():
+
+def workerThreadJpeg():
+    """
+    Converts all ESP32-CAM payloads to `MatLike`s.
+    """
     while True:
         p = s_queueEspPayloads.get()
+
+        if p is None:
+            s_queueEspPayloads.task_done()
+            continue
 
         int8 = np.frombuffer(p, dtype=np.uint8)
         jpeg = cv2.imdecode(int8, cv2.IMREAD_COLOR)
 
-        if jpeg is not None:
-            s_queueJpegs.put(jpeg)
+        if jpeg is None:
+            s_queueEspPayloads.task_done()
+            continue
 
+        s_queueFiles.put(p)
+        s_queueJpegs.put(jpeg)
         s_queueEspPayloads.task_done()
 
 
-def workerThreadWrite():
+def workerThreadDisk():
+    """
+    Writes JPEGs to disk.
+    """
     while True:
-        jpeg = s_queueJpegs.get()
+        payload = s_queueFiles.get()
 
-        with open(f"{s_pathJpegs}/{0}.jpg", "w") as f:
-            f.write(jpeg)
+        if payload is None:
+            s_queueFiles.task_done()
+            continue
 
-        s_queueEspPayloads.task_done()
+        with open(f"{s_pathJpegs}/{0}.jpg", "wb") as f:
+            f.write(payload)
+
+        s_queueFiles.task_done()
 
 
 def workerThreadYolo():
+    """
+    Runs YOLOv11 inference for LPR and notifies Express.
+    """
     while True:
         jpeg = s_queueJpegs.get()
-
         rgb = jpeg[:, :, ::-1]
+        results = s_yolo.predict(source=rgb)
 
-        # s_yolo.predict(source=rgb)
+        if not hasattr(results, "boxes"):
+            s_queueJpegs.task_done()
+            continue
+
+        for box in results.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            confidence = float(box.conf[0])
+            cls = int(box.cls[0])
+            label = results.names[cls]
+            print(f"PLATE NUMBER: `{label}`!")
 
         s_queueJpegs.task_done()
 
 
-async def workerWockFetchEsp(p_ip: str):
-    # print(sys.argv)
-    # ip = sys.argv[0] if len(sys.argv) > 0 else utilEspIpNeigh()
-    # utilEspIpFind()
-    # utilEspIpScan()
-
-    # if not ip:
-    # print("Couldn't see Brahvim's ESP32-CAM in ARP cache just yet, exiting...")
-    # sys.exit(1)
-
-    ip = p_ip
-    print(f"Attempting to connect to ESP32@`{ip}`...")
-    async with websockets.connect(f"ws://{ip}:8000/stream") as wock:
-        async for message in wock:
-            s_queueEspPayloads.put(message)
+def cbckWockMsg(p_ws, p_msg):
+    # print("Received JPEG!")
+    s_queueEspPayloads.put(p_msg)
 
 
 # endregion
 
 if __name__ == "__main__":
     print("Please enter the ESP32's IP address...!: ", end="")
-    uin = input()
+    s_espIpStr = input()
 
-    threading.Thread(target=workerThreadDecode, daemon=True).start()
+    threading.Thread(target=workerThreadDisk, daemon=True).start()
+    threading.Thread(target=workerThreadJpeg, daemon=True).start()
     threading.Thread(target=workerThreadYolo, daemon=True).start()
-    asyncio.run(workerWockFetchEsp(uin))
+
+    websocket.WebSocketApp(
+        f"ws://{s_espIpStr}:80/stream",
+        on_message=cbckWockMsg
+    ).run_forever(),
+
+    # asyncio.run(workerWockFetchEsp(s_espIpStr))
